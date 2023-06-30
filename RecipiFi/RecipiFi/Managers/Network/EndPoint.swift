32522907149b5b6dec3376327768ca62d9bb336b
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-29 22:44:48 -0400

    [Add] - created EndPoint file

---
+++ RecipiFi/RecipiFi/Managers/Network/EndPoint.swift
@@ -0,0 +1,29 @@
+//
+//  EndPoint.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/29/23.
+//
+
+import Foundation
+
+
+struct EndPoint {
+    var path: String
+    var queryItems: [URLQueryItem] = []
+}
+
+extension EndPoint {
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = ""
+        components.path = path
+        components.queryItems = queryItems
+        
+        guard let url = components.url else {
+            preconditionFailure("Invalid URL components: \(components)")
+        }
+        return url
+    }
+}
