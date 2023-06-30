9e292139d554b4e62467609be97b603a24359310
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-30 12:00:14 -0400

    [Add] - created table view diffable data source class

---
+++ RecipiFi/RecipiFi/Modules/Meals/View Models/TableViewDiffableDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewDiffableDataSource.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/30/23.
+//
+
+import Foundation
+import UIKit
+
+
+class TableViewDiffableDataSource: UITableViewDiffableDataSource<String?, Meal> {}
