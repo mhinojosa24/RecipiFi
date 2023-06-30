c674066ff0c13f86a6f3a291997d46c581d0ebad
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-30 12:01:01 -0400

    [Add] - refactored api call method & integrated combine

--- RecipiFi/RecipiFi/Modules/Meals/View Models/MealsVM.swift
+++ RecipiFi/RecipiFi/Modules/Meals/View Models/MealsVM.swift
@@ -6,18 +6,35 @@
 //
 
 import Foundation
+import Combine
+import UIKit
 
 
 class MealsVM: BaseVM {
     
+    var datasource: TableViewDiffableDataSource!
+    var snapshot = NSDiffableDataSourceSnapshot<String?, Meal>()
+    
     func callApiToGetDessertMeals() {
         service?.request(GetDessertMeals()) { result in
             switch result {
-            case .success:
-                break
-            case .failure:
-                break
+            case .success(let meals):
+                if let meals = meals {
+                    self.updateDataSource(with: meals)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
+        }
+    }
+    
+    private func updateDataSource(with meals: [Meal]) {
+        DispatchQueue.main.async {
+            guard self.datasource != nil else { return }
+            self.snapshot.deleteAllItems()
+            self.snapshot.appendSections([""])
+            self.snapshot.appendItems(meals, toSection: "")
+            self.datasource.apply(self.snapshot, animatingDifferences: true)
         }
     }
 }
