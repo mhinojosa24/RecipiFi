48b96f9ee14c78d3f609e2a9ab7e94c024a84eb8
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-30 14:13:08 -0400

    [Add] - created meal detail view controller & view model

---
+++ RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealDetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  MealDetailVC.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/30/23.
+//
+
+import Foundation
+import UIKit
+
+
+class MealDetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RecipiFi/RecipiFi/Modules/Meals/View Models/MealDetailVM.swift
@@ -0,0 +1,13 @@
+//
+//  MealDetailVM.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/30/23.
+//
+
+import Foundation
+
+
+class MealDetailVM: BaseVM {
+    
+}
