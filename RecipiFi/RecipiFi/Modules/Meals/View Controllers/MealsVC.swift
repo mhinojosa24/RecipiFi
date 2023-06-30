bfe342be4f16273323785dd9c1fd5dfe081ef815
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-06-30 11:38:20 -0400

    [Add] - renamed tableview cell name & configured table view UI

--- RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealsVC.swift
+++ RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealsVC.swift
@@ -14,6 +14,21 @@ class MealsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
         navigationController?.view.backgroundColor = .systemBackground
+        let mealCell = UINib(nibName: String(describing: MealCell.self), bundle: nil)
+        tableView.delegate = self
+        tableView.register(mealCell, forCellReuseIdentifier: MealCell.reuseIdentifier)
+        tableView.rowHeight = 100
+    }
+}
+
+
+extension MealsVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
--- RecipiFi/RecipiFi/Modules/Meals/Views/MealCell.swift
+++ RecipiFi/RecipiFi/Modules/Meals/Views/MealCell.swift
@@ -1,5 +1,5 @@
 //
-//  MealsCell.swift
+//  MealCell.swift
 //  RecipiFi
 //
 //  Created by Maximo Hinojosa on 6/30/23.
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class MealsCell: UITableViewCell {
+class MealCell: UITableViewCell {
 
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    static var reuseIdentifier: String { String(describing: self) }
     
     override func awakeFromNib() {
         super.awakeFromNib()
