7b64b073b8424c751ffc9e0567ca41c3729975c7
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-02 04:45:02 -0400

    [Update] - refactored logic

--- RecipiFi/RecipiFi/Managers/Network/Data Model/MealDetail.swift
+++ RecipiFi/RecipiFi/Managers/Network/Data Model/MealDetail.swift
@@ -19,12 +19,15 @@ struct MealDetail: Decodable {
 extension MealDetail {
     func parseIngredientDetail(response: JSONDictionary) -> [IngredientDetail] {
         var ingredientInfo = [IngredientDetail]()
-        for i in 1...20 {
-            let mealID = UUID().uuidString
-            if let ingredient = response["strIngredient\(i)"] as? String, let measurement = response["strMeasure\(i)"] as? String, !ingredient.isEmpty && !measurement.isEmpty {
-                ingredientInfo.append(.init(id: mealID, ingredient: ingredient, measurement: measurement))
-            } else {
-                break
+        for i in 1...20 where !(response["strIngredient\(i)"] as? String ?? "").isEmpty && !(response["strMeasure\(i)"] as? String ?? "").isEmpty {
+            
+            if let ingredient = response["strIngredient\(i)"] as? String,
+               let measurement = response["strMeasure\(i)"] as? String {
+                
+                ingredientInfo.append(.init(id: UUID().uuidString,
+                                            ingredient: ingredient,
+                                            ingredientImageURL: EndPoint.getIngredientImageURL(ingredient: ingredient),
+                                            measurement: measurement))
             }
         }
         return ingredientInfo
