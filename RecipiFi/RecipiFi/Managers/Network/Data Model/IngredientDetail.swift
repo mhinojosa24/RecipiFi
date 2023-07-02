a5522914303b2a1154d01eb5c7e9b88dfe970ea0
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-01 21:24:19 -0400

    [Add] - created new object MealDetail & IngredientDetail

---
+++ RecipiFi/RecipiFi/Managers/Network/Data Model/IngredientDetail.swift
@@ -0,0 +1,14 @@
+//
+//  IngredientDetail.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/30/23.
+//
+
+import Foundation
+
+
+struct IngredientDetail: Decodable {
+    let ingredient: String
+    let measurement: String
+}
---
+++ RecipiFi/RecipiFi/Managers/Network/Data Model/MealDetail.swift
@@ -0,0 +1,30 @@
+//
+//  MealDetail.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 6/30/23.
+//
+
+import Foundation
+
+
+struct MealDetail: Decodable {
+    let idMeal: String?
+    let strMeal: String?
+    let strInstructions: String?
+    var ingredientDetails: [IngredientDetail]?
+}
+
+extension MealDetail {
+    func parseIngredientDetail(response: JSONDictionary) -> [IngredientDetail] {
+        var ingredientInfo = [IngredientDetail]()
+        for i in 1...20 {
+            if let ingredient = response["strIngredient\(i)"] as? String, let measurement = response["strMeasure\(i)"] as? String, !ingredient.isEmpty && !measurement.isEmpty {
+                ingredientInfo.append(.init(ingredient: ingredient, measurement: measurement))
+            } else {
+                break
+            }
+        }
+        return ingredientInfo
+    }
+}
