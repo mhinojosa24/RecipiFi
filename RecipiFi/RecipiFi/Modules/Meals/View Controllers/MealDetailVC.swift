a1385df1faa4b4ab1a117e9af60cbcffdcc10656
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-02 04:46:23 -0400

    [Update] - created method to set UI & logic to update datasource

--- RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealDetailVC.swift
+++ RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealDetailVC.swift
@@ -18,17 +18,25 @@ class MealDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupObservers()
+        viewModel.updateDataSource()
+    }
+    
+    private func setupUI() {
         view.backgroundColor = self.traitCollection.userInterfaceStyle == .dark ? .black : .white
+        let ingredientDetailCell = UINib(nibName: String(describing: IngredientDetailCell.self), bundle: nil)
+        tableView.register(ingredientDetailCell, forCellReuseIdentifier: IngredientDetailCell.reuseIdentifier)
+        tableView.rowHeight = 100
         guard let imageUrl = URL(string: viewModel.model?.strMealThumb ?? "") else { return }
         thumbnailImageView.kf.setImage(with: imageUrl, placeholder: UIImage(named: "photo.fill"), options: [.transition(.fade(1)), .cacheOriginalImage])
         titleLabel.text = viewModel.model?.strMeal ?? ""
     }
     
-    
     private func setupObservers() {
         viewModel.datasource = MealsDetailTableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, model in
             guard let cell = tableView.dequeueReusableCell(withIdentifier: IngredientDetailCell.reuseIdentifier, for: indexPath) as? IngredientDetailCell else { return UITableViewCell() }
-            
+            cell.model = model
             return cell
         })
     }
--- RecipiFi/RecipiFi/Modules/Meals/View Models/MealDetailVM.swift
+++ RecipiFi/RecipiFi/Modules/Meals/View Models/MealDetailVM.swift
@@ -14,4 +14,15 @@ class MealDetailVM: BaseVM {
     var datasource: MealsDetailTableViewDiffableDataSource!
     var snapshot = NSDiffableDataSourceSnapshot<String?, IngredientDetail>()
     var model: MealDetail?
+    
+    func updateDataSource() {
+        DispatchQueue.main.async {
+            guard self.datasource != nil else { return }
+            guard let items = self.model?.ingredientDetails else { return }
+            self.snapshot.deleteAllItems()
+            self.snapshot.appendSections([""])
+            self.snapshot.appendItems(items, toSection: "")
+            self.datasource.apply(self.snapshot, animatingDifferences: true)
+        }
+    }
 }
