74cb463c85bd41ecf7aee332df57b37c09b57cfb
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-03 13:27:38 -0400

    [Add] - created UIConstants for MealsVC

--- RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealsVC.swift
+++ RecipiFi/RecipiFi/Modules/Meals/View Controllers/MealsVC.swift
@@ -9,30 +9,37 @@ import UIKit
 import Combine
 
 class MealsVC: UIViewController {
+    
+    struct UIConstants {
+        let mealCell = UINib(nibName: String(describing: MealCell.self), bundle: nil)
+        let rowHeight: CGFloat = 200
+        let fallbackTableViewCell = UITableViewCell()
+    }
+    
     @IBOutlet var tableView: UITableView!
     
     let viewModel = MealsVM()
+    let uiConstants = UIConstants()
     var subscribers = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        setupUI()
         setupObservers()
         viewModel.callApiToGetDessertMeals()
     }
     
-    private func configureUI() {
+    private func setupUI() {
         navigationController?.view.backgroundColor = .systemBackground
-        let mealCell = UINib(nibName: String(describing: MealCell.self), bundle: nil)
         tableView.delegate = self
-        tableView.register(mealCell, forCellReuseIdentifier: MealCell.reuseIdentifier)
-        tableView.rowHeight = 200
+        tableView.register(uiConstants.mealCell, forCellReuseIdentifier: MealCell.reuseIdentifier)
+        tableView.rowHeight = uiConstants.rowHeight
     }
     
     private func setupObservers() {
         viewModel.datasource = MealsTableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, model in
             guard let cell = tableView.dequeueReusableCell(withIdentifier: MealCell.reuseIdentifier, for: indexPath) as? MealCell else {
-                return UITableViewCell()
+                return self.uiConstants.fallbackTableViewCell
             }
             
             cell.model = model
