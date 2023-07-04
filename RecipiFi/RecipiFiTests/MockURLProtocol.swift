489c01dee8094f878b89f6d2012c149259aee7fc
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-04 16:08:32 -0400

    [Add] - constructed class that handle the loading of protocol specific URL data

---
+++ RecipiFi/RecipiFiTests/MockURLProtocol.swift
@@ -0,0 +1,42 @@
+//
+//  MockURLProtocol.swift
+//  RecipiFiTests
+//
+//  Created by Maximo Hinojosa on 7/4/23.
+//
+
+import XCTest
+
+
+/// This class handles the loading of protocol-specific URL data.
+///  URLProtocol which performs the underlying work of opening network connection, writing the request, and reading back a response.
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("No handler set")
+            return
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {
+    }
+}
