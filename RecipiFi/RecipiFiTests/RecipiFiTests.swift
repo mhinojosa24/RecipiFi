717cd1c2bb00179cda94f329380b541f48703a3f
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-05 08:19:25 -0400

    [Update] - implemented successful XCTest for getting dessert meals

--- RecipiFi/RecipiFiTests/RecipiFiTests.swift
+++ RecipiFi/RecipiFiTests/RecipiFiTests.swift
@@ -9,28 +9,45 @@ import XCTest
 @testable import RecipiFi
 
 final class RecipiFiTests: XCTestCase {
+    var mockService: MockApiService!
+    var mockViewModel: MockViewModel!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        mockService = MockApiService()
+        mockViewModel = MockViewModel(service: mockService)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        mockService = nil
+        mockViewModel = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testGetDessertMeals() throws {
+        let response = HTTPURLResponse(url: mockViewModel.mockGetDessertApiRequest.url,
+                                       statusCode: 200,
+                                       httpVersion: nil,
+                                       headerFields: ["Content-Type": "application/json"]) ?? HTTPURLResponse()
+        
+        let mockData: Data = mockViewModel.loadMockData(type: .dessertMeals) ?? Data()
+        
+        MockURLProtocol.requestHandler = { request in
+            return (response, mockData)
         }
+        
+        let expectation = XCTestExpectation(description: "response")
+        
+        mockViewModel.callApiToGetDessertMeals { result in
+            switch result {
+            case .success(let meals):
+                XCTAssertEqual(meals.first?.idMeal, "53049")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertThrowsError(error)
+            }
+        }
+        wait(for: [expectation], timeout: 2)
     }
-
 }
