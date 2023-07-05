0c267de9a174fb3d718c17308fae6db7594416ec
Maximo Hinojosa <maximohinojosa13@gmail.com>
2023-07-05 08:18:38 -0400

    [Add] - implemented mock view model

---
+++ RecipiFi/RecipiFi/Managers/Mock Network/MockViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  MockViewModel.swift
+//  RecipiFi
+//
+//  Created by Maximo Hinojosa on 7/4/23.
+//
+
+import Foundation
+
+
+class MockViewModel {
+    private(set) var service: MockApiService?
+    
+    let mockGetDessertApiRequest = MockGetDessertApiRequest()
+    let mockGetDessertDetailsApiRequest = MockGetDessertDetailsApiRequest(mealID: "53049")
+    
+    init(service: MockApiService) {
+        self.service = service
+    }
+    
+    func loadMockData(type: MockDataFileName) -> Data? {
+        return service?.loadJsonData(filename: type, extensionType: .json)
+    }
+    
+    func callApiToGetDessertMeals(completionHandler: @escaping (Result<[Meal], Error>) -> Void) {
+        service?.request(mockGetDessertApiRequest, mockFileName: .dessertMeals, completionHandler: { result in
+            switch result {
+            case .success(let meal):
+                if let meal = meal {
+                    completionHandler(.success(meal))
+                }
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        })
+    }
+    
+    func callApiToGetDessertDetails(completionHandler: @escaping (Result<[MealDetail], Error>) -> Void) {
+        service?.request(mockGetDessertDetailsApiRequest, mockFileName: .dessertDetails, completionHandler: { result in
+            switch result {
+            case .success(let mealDetails):
+                break
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        })
+    }
+}
